2fb915a3fd9c01526f4100a6e701b193face5222
voronovdmitry1 <dmitrij.voronov.21@mail.ru>
2024-10-20 14:28:05 +0200

    Add files via upload

---
+++ laba4/laba3.na4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int N = 3, num = 1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", num++);
+        
+        }
+        printf("\n");
+        
+    }
+    return 0;
+    
+}
